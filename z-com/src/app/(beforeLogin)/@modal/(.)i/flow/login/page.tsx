5460ea5b0615025fa0bbc3702ba30e30de957887
hwp2024dev <hwp2024dev@gmail.com>
2026-02-06 20:21:55 +0900

    refactor: SignupModal의 css 경로 수정

--- z-com/src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
+++ z-com/src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
@@ -3,7 +3,6 @@ import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
 export default function Page() {
     return (
         <>
-            <p>인터셉트지롱</p>
             <LoginModal />
         </>
 
